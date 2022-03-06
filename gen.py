1224b800cb7bcba7aa15bc2d13172fbc68584591
Arthur Fait <5372927+arthurfait@users.noreply.github.com>
2022-03-06 17:39:13 +0200

    gen draft added

---
+++ gen.py
@@ -0,0 +1,39 @@
+#!env python3
+import json
+
+TABLE = [
+    {
+        "v1": 1,
+        "rt": 0
+    },
+    {
+        "v1": 1,
+        "rt": 0
+    },
+]
+
+class Application():
+
+    def run(self) -> None:
+        """ App Entry  point """
+        # get rule input
+        # get other input
+
+        if load sceanrio :
+            load sceanrio :
+        else:
+            gen_random()
+
+
+        a = [{"v1": 1}, {"v4": 3}]
+        print(json.dumps(a, indent=4))
+
+
+    d
+
+def main():
+    app = Application()
+    app.run()
+
+if __name__ == "__main__":
+    main()
