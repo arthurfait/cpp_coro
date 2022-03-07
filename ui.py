92367787648ec02c8d58d84cb85122d26aa47187
Arthur Fait <5372927+arthurfait@users.noreply.github.com>
2022-03-07 09:43:21 +0200

    ui

---
+++ ui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+from cgitb import text
+from threading import main_thread
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    greet = tk.Label(text="text is hello", height=20)
+    label = tk.Label(text="Привет, Tkinter!", background="#34A2FE")
+    greet.pack()
+    label.pack()
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
